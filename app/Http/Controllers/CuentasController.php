3665be8f94385ac95bb01d518ec8404002ff2e85
criksman <alvarojg2002@hotmail.com>
2023-06-24 14:42:33 -0400

    Set-up de controladores

---
+++ app/Http/Controllers/CuentasController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Cuenta;
+use Illuminate\Support\Facades\Auth;
+use Gate;
+
+class CuentasController extends Controller
+{
+    //
+}
