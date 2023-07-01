eb9f55c260b236c07f3a806669494399e0677f33
Daniel Saldivar <danielsaldivargonzalez@gmail.com>
2023-06-30 23:50:40 -0400

    aaaa

--- resources/views/artista/create_user.blade.php
+++ resources/views/artista/create_user.blade.php
@@ -5,6 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Vista de Índice</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <style>
+    body{
+            background-color: #311151;
+        }
+
+        .card-background{
+            background-color: #824968;
+            height: 100%;
+            width: 100%;
+        }
+    </style>
 </head>
 <body>
 <div class="card">
